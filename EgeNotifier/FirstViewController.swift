39fc568b21ec7120de9c49101d1a53186d8a7451
Mobikat20 <Mobikat20@kait20.ru>
2018-03-07 11:56:55 +0300

    Initial commit

---
+++ EgeNotifier/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EgeNotifier
+//
+//  Created by Aleksandr Filippov on 07.03.2018.
+//  Copyright © 2018 Kait 20. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
